4da2fe11ee371a2d88ef3799b0154b22ae0c5949
Nathalie CLAUDE <62218338+nathalie9410@users.noreply.github.com>
2026-01-02 18:09:49 +0100

    Update st_rfm_221129.py
    Correction problème de concordance de valeurs sur le diagramme "Evolution des moyens de paiement"

--- st_rfm_221129.py
+++ st_rfm_221129.py
@@ -357,7 +357,7 @@ if page == pages[0]:
       # on construit une LISTE de couleurs dans le bon ordre
       #pie_colors = [colors1[label] for label in counts_pm.index]
 
-      wedges, _ = ax_pie.pie(
+      wedges, texts, autotexts = ax_pie.pie(
          counts_pm.values,
          labels=None,
          autopct="%1.1f%%",
@@ -903,6 +903,7 @@ else:
    st.markdown(''' <style> [data-testid="stMarkdownContainer"] ul{padding-left:40px;} </style> ''', unsafe_allow_html=True)
     
                    
+
 
 
 
